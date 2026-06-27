c37c5c3057a50e63c85723dda6f9dba4f5c45eef
luwuxy <70209257+luwuxy@users.noreply.github.com>
2026-06-27 05:53:45 +0000

    code cleanup

--- app.js
+++ app.js
@@ -19,84 +19,64 @@ function getHumanChoice(input) {
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let output;
 
     function playRound(humanChoice, computerChoice) {
-        /*
-        If humanChoice is equal to computerChoice, print "It's a tie!"
-        Else if humanChoice is equal to rock:
-            If computerChoice is equal to paper, print "You lose! Paper beats Rock."
-            Else if computerChoice is equal to scissors, print "You win! Rock beats Scissors."
-        Else if humanChoice is equal to paper:
-            If computerChoice is equal to rock, print "You win! Paper beats Rock."
-            Else if computerChoice is equal to scissors, print "You lose! Scissors beats Paper."
-        Else if humanChoice is equal to scissors:
-            If computerChoice is equal to rock, print "You lose! Rock beats Scissors."
-            Else if computerChoice is equal to paper, print "You win! Scissors beats Paper."
-         */
         if (humanChoice === computerChoice) {
-            console.log("It's a tie!");
-            return "tie";
+            output = "It's a tie!";
         } else if (humanChoice === "rock") {
             if (computerChoice === "paper") {
-                console.log("You lose! Paper beats Rock.");
+                output = "You lose! Paper beats Rock.";
                 computerScore++;
-                return "loss";
             } else if (computerChoice === "scissors") {
-                console.log("You win! Rock beats Scissors.");
+                output = "You win! Rock beats Scissors.";
                 humanScore++;
-                return "win";
             }
         } else if (humanChoice === "paper") {
             if (computerChoice === "rock") {
-                console.log("You win! Paper beats Rock.");
+                output = "You win! Paper beats Rock.";
                 humanScore++;
-                return "win";
             } else if (computerChoice === "scissors") {
-                console.log("You lose! Scissors beats Paper.");
+                output = "You lose! Scissors beats Paper.";
                 computerScore++;
-                return "loss";
             }
         } else if (humanChoice === "scissors") {
             if (computerChoice === "rock") {
-                console.log("You lose! Rock beats Scissors.");
+                output = "You lose! Rock beats Scissors.";
                 computerScore++;
-                return "loss";
             } else if (computerChoice === "paper") {
-                console.log("You win! Scissors beats Paper.");
+                output = "You win! Scissors beats Paper.";
                 humanScore++;
-                return "win";
             }
         }
     }
 
-    /*
-    While i is less than 5:
-        Assign humanChoice and computerChoice to their respective variables
-        Assign playRound() function to result
-        If result is either a win or a loss, print the scores and increment i.
-        If humanScore reaches 3, break the loop.
-     */
     while (humanScore < 3 && computerScore < 3) {
         const humanSelection = getHumanChoice(prompt("Rock, paper, or scissors?"));
         const computerSelection = getComputerChoice();
-        const result = playRound(humanSelection, computerSelection);
 
-        if (result === "win" || result === "loss") {
-            console.log(
+        if (humanSelection === "invalid") {
+            alert("Invalid input! Please try again.");
+        } else {
+            playRound(humanSelection, computerSelection);
+
+            alert(
+                `${output}\n---\n` +
+                `Your hand: ${humanSelection.toUpperCase()}\n` +
+                `Enemy hand: ${computerSelection.toUpperCase()}`
+            )
+
+            alert(
                 `Your score: ${humanScore}\n` +
                 `AI score: ${computerScore}`
-            )
-        } else if (humanSelection === "invalid") {
-            console.log("Invalid input! Please try again.");
+            );
         }
     }
 
-    if (humanScore === computerScore) {
-        console.log("The score is tied!");
-    } else if (humanScore > computerScore) {
-        console.log("You won the game!");
-    } else if (humanScore < computerScore) {
-        console.log("You lost the game!");
+    if (humanScore > computerScore) {
+        alert("You won the game!");
+    } else {
+        alert("You lost the game!");
     }
 }
 
